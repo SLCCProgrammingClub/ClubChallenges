9eb3dfc87a74fc4dfef1f6d56ebc25acf30c86e3
danistitches <60897785+danistitches@users.noreply.github.com>
2022-10-05 10:19:07 -0600

    Add files via upload

---
+++ Challenge2.java
@@ -0,0 +1,91 @@
+package challenge2;
+
+/**
+ * SLCC Programming Club - Coding Challenge 2
+ * October 5, 2022
+ * 
+ * This week's coding challenges feature common interview questions that require recursion.
+ * Implement the methods below based off of the instructions provided in the doc comments.
+ * 
+ * For an added challenge, try writing out your code before using your computer. Type it into
+ * your computer only when you're ready to verify your code works!
+ * 
+ * @author ...
+ *
+ */
+public class Challenge2 {
+	
+	/**
+	 * Creates a new string by removing any instance of the character 'x' from a 
+	 * provided String. 
+	 * 
+	 * Warm up by coding this iteratively or recursively.
+	 * 
+	 * EXAMPLES:
+	 * warmUp("axbxcxd") → "abcd"
+	 * warmUp("xxx a xxx") → " a " 
+	 * 
+	 * @param str string to modify
+	 * @return string with x's removed
+	 */
+	public String warmUp(String str) {
+		return ""; //TODO
+	}
+
+	/**
+	 * Reverses a given string.
+	 * 
+	 * EXAMPLES:
+	 * reverse("hello") → "olleh"
+	 * reverse("extraordinary") → "yranidroartxe"
+	 * 
+	 * @param str string to reverse
+	 * @return reversed string
+	 */
+	public String reverse (String str) {
+		return ""; //TODO
+	}
+	
+	/**
+	 * Determines the sum of two given numbers without using any arithmetic operators.
+	 * 
+	 * EXAMPLES:
+	 * sum(12,15) → 27
+	 * sum(20, 3) → 23
+	 * 
+	 * @param first first number
+	 * @param second second number
+	 * @return sum of the two numbers
+	 */
+	public static int sum(int first, int second) {
+		return 0; //TODO
+	}
+	
+	/**
+	 * ULTIMATE CHALLENGE:
+	 * 
+	 * Returns the longest substring present in the provided Strings. Empty Strings are
+	 * valid.
+	 * 
+	 * EXAMPLES:
+	 * longestSubstring("Fresh","Press") → "res"
+	 * longestSubstring("solution","action") → "tion"
+	 * 
+	 * @param a first string
+	 * @param b second string
+	 * @return longest substring
+	 */
+	public String longestSubstring(String first, String second) {
+		return ""; //TODO
+	}
+	
+	/** ===================================================================================
+	 *  TEST CLIENT
+	 *  ===================================================================================
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
